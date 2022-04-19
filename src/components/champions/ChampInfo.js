c7a1172560cb97c10a44fc864aeb5472f0a46e4b
leeheeae <chromeee@daum.net>
2022-04-20 01:35:45 +0900

    스킬 컴포넌트 제작

--- src/components/champions/ChampInfo.js
+++ src/components/champions/ChampInfo.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import Response from '../common/Response';
+import Skill from './Skill';
 import data from '../../db/data.json';
 
 const ChampInfoBox = styled.div`
+  margin-top: 50px;
   display: flex;
+  align-items: flex-end;
+
+  .champ-name {
+    font-size: 1.6rem;
+  }
+`;
+
+const InfoBox = styled.div`
+  margin-left: 30px;
+`;
+
+const SkillList = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+
+  > div + div {
+    margin-left: 10px;
+  }
 `;
 
 const ChampInfo = () => {
@@ -15,13 +36,20 @@ const ChampInfo = () => {
     <>
       <Response>
         <ChampInfoBox>
-          <div>
+          <div className="champ-img">
             <img
               src={`http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/${champ.image.full}`}
               alt="img"
             />
           </div>
-          <div>이름 : {champ.name}</div>
+          <InfoBox>
+            <h2 className="champ-name"> {champ.name}</h2>
+            <SkillList>
+              <Skill skill={champ.spells[0]} />
+              <Skill skill={champ.spells[1]} />
+              <Skill skill={champ.spells[2]} />
+            </SkillList>
+          </InfoBox>
         </ChampInfoBox>
       </Response>
     </>
---
+++ src/components/champions/Skill.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SkillBox = styled.div`
+  position: relative;
+  width: 50px;
+  height: 50px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 3px;
+  }
+
+  .skill-key {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 20px;
+    height: 20px;
+    font-size: 13px;
+    font-weight: 400;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0.6);
+    color: #fff;
+  }
+  .skill-text {
+    display: none;
+    width: 250px;
+    padding: 1.1rem;
+    position: absolute;
+    top: 60px;
+    z-index: 2;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    line-height: 1.4;
+    font-size: 12px;
+    font-weight: 300;
+
+    .spell-name {
+      color: #f56d91;
+      font-size: 14px;
+      margin-bottom: 8px;
+    }
+    .spell-description {
+      margin-top: 8px;
+    }
+  }
+
+  &:hover .skill-text {
+    display: block;
+  }
+`;
+
+const Skill = ({ skill }) => {
+  return (
+    <SkillBox>
+      <img
+        src={`
+              https://ddragon.leagueoflegends.com/cdn/12.7.1/img/spell/${skill.image.full}`}
+        alt="img"
+      />
+      <div className="skill-key">{skill.id.substr(-1)}</div>
+      <div className="skill-text">
+        <h4 className="spell-name">{skill.name}</h4>
+        <div>{skill.cooldown}</div>
+        <div>소모: {skill.costType}</div>
+        <div>범위: {skill.rangeBurn}</div>
+        <div className="spell-description">{skill.description}</div>
+      </div>
+    </SkillBox>
+  );
+};
+
+export default Skill;
