00db6af6c52df18246783b014c64bd597b5df1de
leeheeae <chromeee@daum.net>
2022-04-25 22:25:27 +0900

    챔피언 검색/ 전체 api 수정

--- src/lib/api/champ.js
+++ src/lib/api/champ.js
@@ -8,10 +8,17 @@ const config = {
 };
 
 //챔피언 전체 검색
-export const summonerByNameSearch = () =>
-  client.get(`${config.url.ddragon}/champion.json`).then(({ data }) => {
-    return data;
-  });
+export const summonerByNameSearch = async (champname) => {
+  if (champname) {
+    return await client.get(`${config.url.ddragon}/champion/${champname}.json`);
+  } else {
+    return await client
+      .get(`${config.url.ddragon}/champion.json`)
+      .then(({ data }) => {
+        return data;
+      });
+  }
+};
 
 //로테이션 챔피언 검색
 export const championLotaionsSearch = () =>
